9661ce5256dba2704ed43985fa3e3faa2533cd84
Rui Silva <rsilva6420@gmail.com>
2023-06-06 11:23:23 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	let [todo, setTodo] = useState('');
+	let [todoList, setTodoList] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="container text-center pt-3">
+			<h1>My ToDo List</h1>
+			</div>
+			<div className="d-flex justify-content-center pt-3">
+				<input 
+					className="me-2"
+					type="text"
+					placeholder="Enter a task"
+					value={todo}
+					onChange={(e) => {
+						setTodo(e.target.value);
+					}}
+				/>
+				<button onClick={() => {
+					setTodoList([...todoList, todo]);
+					setTodo('');
+				}}>
+					Add ToDo
+				</button>
+			</div>
+			<div className="d-flex justify-content-center mt-3">
+				<ul className="pe-5">
+					{todoList.length !== 0 && todoList.map((todoItem) => {
+						return <li>{todoItem}</li>;
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };
